3fa8eb3374ed4c1fbfa5c0fc4f58212f513d32d5
Hritik <hritik4november2003@gmail.com>
2024-01-07 17:48:46 +0530

    p

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Landing from './landing_page';
-import About from '../../../src/about';
-import Projects from '../../../src/projects';
-import Skills from '../../../src/skills';
-import Profiles from '../../../src/profiles';
-import Contact from '../../../src/contact';
+//import Landing from './landing_page.js';
+import About from './about.js';
+import Projects from './projects.js';
+import Skills from './skills.js';
+import Profiles from './profiles.js';
+import Contact from './contact.js';
 import video from './back1.mp4'
 function App(){
   return(
   <div>
-    <Landing />
+    
     <About />
     <Projects />
     <Skills />
